58dd4e60a0c7d997c81474ede1ffe7c7cb5d4dfd
iasfand <asfandalicloud786@outlook.com>
2024-11-25 16:53:45 +0000

    init basic Flask application

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.routes.main import main
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+
+    # Register routes
+    app.register_blueprint(main)
+
+    return app
