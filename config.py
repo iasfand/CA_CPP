0e6f61d9307bb2716b0138ad8a76c67e97f50930
iasfand <asfandalicloud786@outlook.com>
2024-11-29 02:37:16 +0000

    add environment variable loading and API keys to config

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("ENV", "DEVT")
+    OPENAIP_API_KEY = os.environ.get("OPENAIP_API_KEY")
+    OPENAIP_API_URL = os.environ.get("OPENAIP_API_URL")
+    AIRPORTDB_API_KEY = os.environ.get("AIRPORTDB_API_KEY")
+
