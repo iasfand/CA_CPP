fca12e1d7a253a38e9278f5127e5f77d8045591b
iasfand <asfandalicloud786@outlook.com>
2024-11-30 22:01:07 +0000

    replace AIRPORTDB_API_KEY with SESSION_SECRET in configuration

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     SECRET_KEY = os.environ.get("ENV", "DEVT")
     OPENAIP_API_KEY = os.environ.get("OPENAIP_API_KEY")
     OPENAIP_API_URL = os.environ.get("OPENAIP_API_URL")
-    AIRPORTDB_API_KEY = os.environ.get("AIRPORTDB_API_KEY")
+    SESSION_SECRET = os.environ.get("SESSION_SECRET")
 
