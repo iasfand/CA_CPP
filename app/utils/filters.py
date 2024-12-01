fe27592935368b75d31e240eb31bce5c23e71401
iasfand <asfandalicloud786@outlook.com>
2024-12-01 02:50:22 +0000

    feat: add format_date_time filter for improved date-time formatting

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -6,3 +6,5 @@ def format_date(value, format="%B %d, %Y"):
         value = datetime.datetime.fromisoformat(value)
     return value.strftime(format)
 
+def format_date_time(value, format="%Y-%m-%d %H:%M:%S"):
+    return datetime.datetime.fromisoformat(value).strftime(format)
