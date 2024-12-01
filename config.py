33307396a67c37b2d1ed21e40e8e88681bdc723c
Anis <me.anis.dev@gmail.com>
2024-12-01 16:57:30 +0000

    feat: update configuration for AWS credentials and add example .env file

--- config.py
+++ config.py
@@ -3,17 +3,23 @@ import os
 
 load_dotenv()
 
+os.environ['AWS_SHARED_CREDENTIALS_FILE'] = './.aws/credentials'
+os.environ['AWS_CONFIG_FILE'] = './.aws/config'
+
 class Config:
-    SECRET_KEY = os.environ.get("ENV", "DEVT")
+    # General App Configuration
+    MODE = os.environ.get("MODE", "DEVT")
+    FLASK_RUN_PORT = os.environ.get("FLASK_RUN_PORT", 5000)
     OPENAIP_API_KEY = os.environ.get("OPENAIP_API_KEY")
     OPENAIP_API_URL = os.environ.get("OPENAIP_API_URL")
     SESSION_SECRET = os.environ.get("SESSION_SECRET")
-    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
-    AWS_REGION = os.getenv("AWS_REGION")
-    AWS_PROFILE_NAME = os.getenv("AWS_PROFILE_NAME")
-    DYNAMODB_TABLE_NAME = "skyport_users"
-    DYNAMODB_FAVS_TABLE_NAME = "skyport_favs"
-    S3_BUCKET_NAME = "skyport-bucket"
-    DYNAMODB_MEMO_TABLE_NAME = "skyport_memories"
+
+    # AWS Credentials and Configuration
+    AWS_PROFILE_NAME = os.getenv("AWS_PROFILE_NAME", "250738637992_MSCCLOUD")
+    AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+
+    # AWS Services Configuration
+    DYNAMODB_TABLE_NAME = os.getenv("DYNAMODB_TABLE_NAME", "skyport_users")
+    DYNAMODB_FAVS_TABLE_NAME = os.getenv("DYNAMODB_FAVS_TABLE_NAME", "skyport_favs")
+    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "skyport-bucket")
+    DYNAMODB_MEMO_TABLE_NAME = os.getenv("DYNAMODB_MEMO_TABLE_NAME", "skyport_memories")
